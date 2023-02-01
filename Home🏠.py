c5189e0c7578ad565cb2bc3a2bcdaa588de1937a
nghi97 <79874430+nghi97@users.noreply.github.com>
2023-02-01 16:40:54 +0100

    Add files via upload

---
+++ Home🏠.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="FNE Master ENSP 2022-2023", page_icon="‍trophy")
+st.sidebar.markdown("# Home :house:")
+st.title(" Formation des enseignants et enseignants-chercheurs au numérique :book:")
+st.markdown(":blue[Réalisé par le Master ENSP 2022-2023]")
+st.markdown("Pilotage du projet : **Jean-Yves Jeannas**, Chargé de mission Culture numérique, Université de Lille. ")
+st.markdown("Coordination opérationnelle : **:blue[Téodorina Tibar]**, chef de projet web, DAPI (Direction d’appui à la pédagogie et à l’innovation), **représentante de l’équipe projet auprès des M2 ENSP.**")
+st.subheader("Présentation du projet")
+st.write("Durant notre M2 ENSP, nous menons un projet collectif qui a pour objectif de répondre à une commande. Cette année, cette commande se déroule dans le cadre du projet FNE (Formation des enseignants et enseignants-chercheurs au numérique) financé par le Plan Nationale de Relance et Résilience. Le but global est de rendre compte de la montée en compétences des enseignant.e.s-chercheur.se.s de l’Université de Lille dans l’utilisation des outils et services numériques, en particulier d’un point de vue pédagogique. Ce projet traite donc de l’exploitation des données et de la visualisation des résultats individuels.")
+
+
