c5088ce23b2a405a4a644cb77899d9158409de9e
nghi97 <79874430+nghi97@users.noreply.github.com>
2023-02-07 00:43:52 +0100

    Update Compétence🏅.py

--- pages/Compétence🏅.py
+++ pages/Compétence🏅.py
@@ -78,8 +78,8 @@ text_colors = ["#000000"] * 5
 def plot(names):
     min_range = [0, 0, 0, 0, 0]
     max_range = [100, 100, 100, 100, 100]
-    for name in range(len(names)-1):
-        rep=scoring.loc[scoring['Nom.complet']==name,"domaine_01":"domaine_05"].values.tolist()[0]
+    #for name in range(len(names)-1):
+        #rep=scoring.loc[scoring['Nom.complet']==name,"domaine_01":"domaine_05"].values.tolist()[0]
     baker = PyPizza(
         params=domaines,           # list of parameters
         min_range=min_range,        # min range values
@@ -93,7 +93,7 @@ def plot(names):
         
         )
     fig, ax = baker.make_pizza(
-        rep,
+        scoring.loc[scoring['Nom.complet'].isin(name),"domaine_01":"domaine_05"].values.tolist()[0],
         figsize=(12, 13),                # adjust figsize according to your need
         color_blank_space="same",        # use same color to fill blank space
         slice_colors=slice_colors,       # color for individual slices
